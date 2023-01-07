5f7fbecd66ba8e8aa5fa696ad3026d13568c69bd
Djneto <dijalma.bneto@gmail.com>
2023-01-06 21:19:57 -0300

    Solução do desafio

--- src/index.js
+++ src/index.js
@@ -10,19 +10,62 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find( user => user.username === username);
+
+  if(!user){
+    return response.status(404).json({ error: "Not Found"});
+  } else{
+    request.user = user;
+    return next();
+  }
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if( (user.pro === false && user.todos.length < 10) || user.pro === true){
+    return next();
+  } else {
+    return response.status(403).json({ error: "Plan not avaiable!"});
+  }
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  if(validate(id)){
+    const user = users.find( user => user.username === username);
+    if(!user){
+      return response.status(404).json({ error: "Not Found"});
+    } else{
+      const todo = user.todos.find( todo => todo.id === id);
+      if(!todo){
+        return response.status(404).json({ error: "Not Found"});
+      } else {
+        request.todo = todo;
+        request.user = user;
+        return next();
+      }
+    }
+  } else {
+    return response.status(400).json({ error: "Not UUID"})
+  }
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find( user => user.id === id);
+
+  if(!user){
+    return response.status(404).json({ error: "User not found!"});
+  } else{
+    request.user = user;
+    return next();
+  }
 }
 
 app.post('/users', (request, response) => {
